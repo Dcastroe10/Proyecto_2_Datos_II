884d11b7f0834dc9096cb05ac97e62c8494a7255
Dcastroe10 <dcastroeli@gmail.com>
2022-05-17 00:36:44 -0600

    Avance circulos

--- PaintPlusPlus/Tools/circle.cpp
+++ PaintPlusPlus/Tools/circle.cpp
@@ -10,13 +10,61 @@ circle::circle()
 
 void circle::draw_circle(pixel **canvas, int start[2], int end[2], uint32_t color, int grosor){
     int radius = abs((start[0]-end[0])/2);
-    int center;
+    int centerx;
+    int centery;
+    int grados = 0; // para probar hacer la vuelta
     if (start[0] < end[0]){
-        center = start[0]+radius;
+        centerx = start[0]+radius;
     }else{
-        center = start[0] - radius;
+        centerx = start[0] - radius;
     }
+
+    if(start[1]< end[1]){
+        centery = start[1] + radius;
+
+    }else{
+        centery = start[1] - radius;
+    }
+
+
+
     qDebug()<<"entroooooooooooooo    "<<radius<<"      radioooo";
-    qDebug()<<"entroooooooooooooo    "<<center<<"      centeeeer";
+    qDebug()<<"entroooooooooooooo    "<<centerx<<"      centeeeer";
+
+    for (int i = -grosor; i < grosor; i++){
+        for(int j = -grosor; j < grosor; j++){
+            canvas[centerx + i][centery + j].setColor(color);
+
+            //En x
+            canvas[centerx + radius + i][centery + j].setColor(color);
+            canvas[centerx - radius + i][centery + j].setColor(color);
+            //EN Y
+            canvas[centerx + i][centery + radius + j].setColor(color);
+            canvas[centerx + i][centery - radius + j].setColor(color);
+
+            canvas[centerx + i + 5][centery + radius + j].setColor(color);
+            canvas[centerx + i + 5][centery - radius + j].setColor(color);
+        }
+    }
+
+
+
 
 }
+
+
+
+
+/*
+ *     for(int x = startx; x<endx;x++){
+        for (int i = -grosor; i < grosor; i++){
+            for(int j = -grosor; j < grosor; j++){
+                canvas[ciclo_startx + i][ciclo_starty + j].setColor(color);
+                canvas[ciclo_startx + i][ciclo_endy + j].setColor(color);
+
+
+            }
+        }
+        ciclo_startx+=1;
+    }
+    */
